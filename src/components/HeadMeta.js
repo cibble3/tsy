b161962ebc3ff364dc81a44eda6fb240b8e36aa0
MalayP18 <malay.conceptive@gmail.com>
2023-06-23 14:59:52 +0530

    fixed meta missing values

--- src/components/HeadMeta.js
+++ src/components/HeadMeta.js
@@ -4,8 +4,8 @@ import { useRouter } from "next/router";
 
 const HeadMeta = ({ pageContent }) => {
   const { asPath } = useRouter();
-  const canonicalUrl =
-    typeof window !== "undefined" ? window.location.origin + asPath : "";
+  const siteUrl = typeof window !== "undefined" ? window.location.origin : "";
+  const canonicalUrl = typeof siteUrl !== "undefined" ? siteUrl + asPath : "";
 
   // console.log(window.location.origin + asPath);
   return (
@@ -15,7 +15,10 @@ const HeadMeta = ({ pageContent }) => {
       <meta name="Rating" content="Mature" />
       <meta name="dc.language" content="US" />
       <meta name="description" content={pageContent.meta_desc} />
-      <meta name="keywords" content={pageContent.meta_keywords} />
+      {pageContent.meta_keywords && (
+        <meta name="keywords" content={pageContent.meta_keywords} />
+      )}
+
       <meta name="robots" content="index, follow" />
       <meta name="theme-color" content="#3d3d3d" />
       <meta
@@ -27,25 +30,25 @@ const HeadMeta = ({ pageContent }) => {
       <meta name="dc.source" content="" />
       <meta name="dc.title" content={pageContent.meta_title} />
       <meta property="dc.keywords" content={pageContent.meta_keywords} />
-      <meta name="dc.subject" content="" />
+      <meta name="dc.subject" content={pageContent.meta_desc} />
       <meta name="dc.description" content={pageContent.meta_desc} />
       {/* <!--item prop--> */}
-      <meta itemProp="name" content="" />
+      {/* <meta itemProp="name" content="" /> */}
       <meta itemProp="description" content={pageContent.meta_desc} />
-      <meta itemProp="image" content="" />
+      <meta itemProp="image" content={siteUrl + "/logo.png"} />
       {/* <!--Facebook meta tags--> */}
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content="TsYum.com" />
-      <meta property="og:url" content="" />
+      <meta property="og:url" content={canonicalUrl} />
       <meta property="og:title" content={pageContent.meta_title} />
       <meta property="og:description" content={pageContent.meta_desc} />
       {/* <!--Twitter meta cards--> */}
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="" />
-      <meta name="twitter:creator" content="" />
+      <meta name="twitter:site" content={canonicalUrl} />
+      <meta name="twitter:creator" content="@tsstream" />
       <meta name="twitter:title" content={pageContent.meta_title} />
       <meta name="twitter:description" content={pageContent.meta_desc} />
-      <meta name="twitter:image" content="" />
+      <meta name="twitter:image" content={siteUrl + "/logo.png"} />
       <link rel="canonical" href={canonicalUrl} />
     </Head>
   );
